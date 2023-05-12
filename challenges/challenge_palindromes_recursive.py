4a0e092c8ccb9372f61c060f5e40ff8093efdb42
Guilherme Borges <guidz.2004@gmail.com>
2023-05-11 22:40:17 -0300

    feat: implement verify palindrome function

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_recursive(word='', low_index=0, high_index=None):
+    if word == '':
+        return False
+
+    if high_index is None:
+        high_index = len(word) - 1
+
+    if low_index >= high_index:
+        return True
+
+    if word[high_index] == word[low_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+    return False
