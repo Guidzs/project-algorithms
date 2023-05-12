7d392c8b93a3e68c2bf19e2ffe2f512bdc964d0e
Guilherme Borges <guidz.2004@gmail.com>
2023-05-11 21:47:05 -0300

    feat: implement function for get study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    frequency = 0
+    if target_time is None:
+        return None
+
+    for period in permanence_period:
+        if not type(period[0]) == int or not type(period[1]) == int:
+            return None
+        if period[0] <= target_time <= period[1]:
+            frequency += 1
+
+    return frequency
