ba4a8a79f8e1bd10a97d0c8d3ae6116080fbef51
Guilherme Borges <guidz.2004@gmail.com>
2023-05-15 18:54:58 -0300

    refact: solves complexity problem

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -3,11 +3,11 @@ def find_duplicate(nums):
         isinstance(n, int) for n in nums
     ) or len(nums) <= 1:
         return False
-
-    for n in nums:
+    nums.sort()
+    for i, n in enumerate(nums):
         if n < 0:
             return False
-        if list.count(nums, n) > 1:
+        if i <= (len(nums) - 2) and n == nums[i + 1]:
             return n
 
     return False
