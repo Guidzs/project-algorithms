f63a667a92c51fc79ec38b27746e63217b1092e2
Guilherme Borges <guidz.2004@gmail.com>
2023-05-15 17:11:25 -0300

    feat: implement find duplicate num function

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not all(
+        isinstance(n, int) for n in nums
+    ) or len(nums) <= 1:
+        return False
+
+    for n in nums:
+        if n < 0:
+            return False
+        if list.count(nums, n) > 1:
+            return n
+
+    return False
